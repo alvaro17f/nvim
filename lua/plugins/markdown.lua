499f334128f896fa4105ca58ebd0a608774708cf
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-09-15 20:48:26 +0200

    refactor(markdown): extract plugin to file

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,11 @@
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons", --"echasnovski/mini.icons",
+	},
+	opts = {
+		file_types = { "markdown", "Avante" },
+	},
+	ft = { "markdown", "Avante" },
+}

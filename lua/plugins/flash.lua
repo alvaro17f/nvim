79e7803627ab45e48d9486c8e93e15710e0a68f0
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-09-17 08:59:30 +0200

    feat(flash): enable search

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -3,7 +3,13 @@ return {
   event = "VeryLazy",
   ---@diagnostic disable-next-line: undefined-doc-name
   ---@type Flash.Config
-  opts = {},
+  opts = {
+    modes = {
+      search = {
+        enabled = true,
+      },
+    },
+  },
   --stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },

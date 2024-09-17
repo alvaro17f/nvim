4343baffca20185fff3caf7e181454c7b5dab5d0
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-09-17 10:15:37 +0200

    feat: add treesitter autotag

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,26 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
   event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
   config = function()
     local treesitter = require("nvim-treesitter.configs")
+    local autotag = require("nvim-ts-autotag")
+
     treesitter.setup({
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
+    })
+
+    autotag.setup({
+      opts = {
+        enable_close = true, -- Auto close tags
+        enable_rename = true, -- Auto rename pairs of tags
+        enable_close_on_slash = false, -- Auto close on trailing </
+      },
     })
   end,
 }

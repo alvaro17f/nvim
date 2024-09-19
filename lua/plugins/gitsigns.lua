a00c189fe4491cf0243d01f9611dfa781e57a3ae
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-09-19 20:55:41 +0200

    feat(gitsigns): remap keys

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,13 +4,35 @@ return {
   keys = {
     {
       mode = "n",
-      "<leader>gp",
+      "g;",
       ":Gitsigns preview_hunk_inline<CR>",
       silent = true,
-      desc = "Gitsigns Preview Hunk",
+      desc = "Gitsigns preview hunk inline",
+    },
+    {
+      mode = "n",
+      "g[",
+      ":Gitsigns prev_hunk<CR>",
+      silent = true,
+      desc = "Gitsigns previous hunk",
+    },
+    {
+      mode = "n",
+      "g]",
+      ":Gitsigns next_hunk<CR>",
+      silent = true,
+      desc = "Gitsigns next hunk",
     },
   },
   opts = {
     current_line_blame = true,
+    sign_priority = 0,
+    signs = {
+      add = { text = "+" },
+      change = { text = "~" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+    },
   },
 }

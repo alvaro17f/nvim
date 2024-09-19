eb39a4b8db0c4d52c25b3ef4c38d264ff671ec50
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-09-19 17:00:45 +0200

    feat(noice): remap keys

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,22 +1,19 @@
 -- https://github.com/folke/noice.nvim/wiki/A-Guide-to-Messages#showmode
 return {
   "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {},
-  keys = {
-    {
-      mode = "n",
-      "<leader>d",
-      "<CMD>Noice dismiss<CR>",
-      { silent = true, pending = true },
-      desc = "Dismiss all Notifications",
-    },
-  },
   dependencies = {
     "MunifTanjim/nui.nvim",
     --"rcarriga/nvim-notify", --if you want to use the notification view.
   },
-  config = function()
-    require("noice").setup()
-  end,
+  event = "VeryLazy",
+  keys = {
+    {
+      mode = "n",
+      "<leader>nd",
+      ":Noice dismiss<CR>",
+      { silent = true, pending = true },
+      desc = "Dismiss all Notifications",
+    },
+  },
+  opts = {},
 }

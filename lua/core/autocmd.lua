086a48ceca5c25661b5f3f68856baf1182e2826c
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-09-20 08:31:51 +0200

    feat(autocmd): highlight on yank

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -31,6 +31,17 @@ vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
 })
 
 ------------------------------------
+--HIGHLIGHT ON YANK
+------------------------------------
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+------------------------------------
 --DEPENDENCIES
 ------------------------------------
 require("utils.dependencies").missing_dependencies_notification()

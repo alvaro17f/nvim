155b1e77df3b76a8645f0edab74dd9514f2efcf0
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-10-02 16:42:25 +0200

    feat(options): add shiftround

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,17 +3,15 @@
 ------------------------------------
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+------------------------------------
 vim.g.have_nerd_font = false
-
-vim.schedule(function()
-  vim.opt.clipboard = "unnamedplus"
-end)
+------------------------------------
 vim.opt.autochdir = true
 vim.opt.backup = false
 vim.opt.breakindent = true
 vim.opt.cmdheight = 1
--- vim.opt.colorcolumn = "80"
-vim.opt.completeopt = "menuone,noselect"
+vim.opt.colorcolumn = "80"
+vim.opt.completeopt = "menu,menuone,noselect"
 vim.opt.cursorline = true
 vim.opt.expandtab = true
 vim.opt.hidden = true
@@ -28,6 +26,7 @@ vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 999
+vim.opt.shiftround = true
 vim.opt.shiftwidth = 2
 vim.opt.shortmess:append({ c = true })
 vim.opt.showmode = false
@@ -45,3 +44,8 @@ vim.opt.termguicolors = true
 vim.opt.timeoutlen = 300
 vim.opt.updatetime = 250
 vim.opt.wrap = false
+------------------------------------
+vim.schedule(function()
+  vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
+end)
+------------------------------------

70d44df4821edbdd09599b226607c23e34843169
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-10-03 13:49:16 +0200

    feat(alpha): add new header

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -9,25 +9,6 @@ return {
     local dashboard = require("alpha.themes.dashboard") -- ["dashboard", "startify"]
 
     dashboard.section.header.val = {
-      [[                                                                          ]],
-      [[   .S_SSSs    S.       .S    S.    .S_SSSs     .S_sSSs      sSSs_sSSs     ]],
-      [[  .SS~SSSSS   SS.     .SS    SS.  .SS~SSSSS   .SS~YS%%b    d%%SP~YS%%b    ]],
-      [[  S%S   SSSS  S%S     S%S    S%S  S%S   SSSS  S%S   `S%b  d%S'     `S%b   ]],
-      [[  S%S    S%S  S%S     S%S    S%S  S%S    S%S  S%S    S%S  S%S       S%S   ]],
-      [[  S%S SSSS%S  S&S     S&S    S%S  S%S SSSS%S  S%S    d*S  S&S       S&S   ]],
-      [[  S&S  SSS%S  S&S     S&S    S&S  S&S  SSS%S  S&S   .S*S  S&S       S&S   ]],
-      [[  S&S    S&S  S&S     S&S    S&S  S&S    S&S  S&S_sdSSS   S&S       S&S   ]],
-      [[  S&S    S&S  S&S     S&S    S&S  S&S    S&S  S&S~YSY%b   S&S       S&S   ]],
-      [[  S*S    S&S  S*b     S*b    S*S  S*S    S&S  S*S   `S%b  S*b       d*S   ]],
-      [[  S*S    S*S  S*S.    S*S.   S*S  S*S    S*S  S*S    S%S  S*S.     .S*S   ]],
-      [[  S*S    S*S   SSSbs   SSSbs_S*S  S*S    S*S  S*S    S&S   SSSbs_sdSSS    ]],
-      [[  SSS    S*S    YSSP    YSSP~SSS  SSS    S*S  S*S    SSS    YSSP~YSSY     ]],
-      [[         SP                              SP   SP                          ]],
-      [[         Y                               Y    Y                           ]],
-      [[                                                                          ]],
-    }
-
-    dashboard.section.header.val = {
       [[                                                                       ]],
       [[                                                                     ]],
       [[       ████ ██████           █████      ██                     ]],
@@ -40,6 +21,20 @@ return {
       [[                                                                       ]],
       [[                                                                       ]],
       [[                                                                       ]],
+    }
+
+    dashboard.section.header.val = {
+      [[        .---.        .----------- ]],
+      [[       /     \  __  /    ------   ]],
+      [[      / /     \(  )/    -----     ]],
+      [[     //////   ' \/ `   ---        ]],
+      [[    //// / // :    : ---          ]],
+      [[   // /   /  /`    '--            ]],
+      [[  //          //..\\              ]],
+      [[         ====UU====UU====         ]],
+      [[             '//||\\`             ]],
+      [[               ''``               ]],
+      [[           memento mori           ]],
     }
 
     dashboard.section.buttons.val = {

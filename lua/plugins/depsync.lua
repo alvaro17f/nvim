302a74a870a52e479a296df434306c40a973d155
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-11-01 10:22:15 +0100

    feat: add cargo to depsync

--- lua/plugins/depsync.lua
+++ lua/plugins/depsync.lua
@@ -4,7 +4,7 @@ return {
     require("depsync").setup()
     vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
       group = vim.api.nvim_create_augroup("depsync", {}),
-      pattern = "package.json",
+      pattern = { "package.json", "Cargo.toml" },
       callback = function()
         vim.cmd("DepSync")
       end,

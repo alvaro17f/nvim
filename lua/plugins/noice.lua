feaeab03bb43bb8844ea6a060807beb9f2aa8da9
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-11-19 10:01:12 +0100

    feat(noice): add keymap to show all notifications

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -3,10 +3,17 @@ return {
   "folke/noice.nvim",
   dependencies = {
     "MunifTanjim/nui.nvim",
-    --"rcarriga/nvim-notify", --if you want to use the notification view.
+    -- "rcarriga/nvim-notify", --if you want to use the notification view.
   },
   event = "VeryLazy",
   keys = {
+    {
+      mode = "n",
+      "<leader>nn",
+      ":NoiceTelescope<CR>",
+      { silent = true, pending = true },
+      desc = "Show all Notifications",
+    },
     {
       mode = "n",
       "<leader>nd",

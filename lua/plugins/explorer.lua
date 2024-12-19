533ac020c474dcf9f39a01bacb9c51dde30a403e
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-12-19 09:28:09 +0100

    feat(yazi): add keybindings

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -10,7 +10,20 @@ if vim.fn.executable("yazi") == 1 then
       open_for_directories = false,
       keymaps = {
         show_help = "<f1>",
+        open_file_in_vertical_split = "<c-w>v",
+        open_file_in_horizontal_split = "<c-w>s",
+        open_file_in_tab = false,
+        grep_in_directory = false,
+        replace_in_directory = "<c-r>",
+        cycle_open_buffers = false,
+        copy_relative_path_to_selected_files = "<c-y>",
+        send_to_quickfix_list = "<c-q>",
+        change_working_directory = ";",
       },
+    },
+    future_features = {
+      ya_emit_reveal = true,
+      ya_emit_open = true,
     },
   }
 end

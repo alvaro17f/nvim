b5c68ba2fb37fa90400060d65120e4f830e63358
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-01-08 18:16:35 +0100

    feat(treesitter): add tressitter context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,17 @@
+---@diagnostic disable: missing-fields
+
 return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
     "windwp/nvim-ts-autotag",
+    "nvim-treesitter/nvim-treesitter-context",
   },
   event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
   config = function()
     local treesitter = require("nvim-treesitter.configs")
     local autotag = require("nvim-ts-autotag")
+    local context = require("treesitter-context")
 
     treesitter.setup({
       auto_install = true,
@@ -21,6 +25,11 @@ return {
         enable_rename = true, -- Auto rename pairs of tags
         enable_close_on_slash = false, -- Auto close on trailing </
       },
+    })
+
+    context.setup({
+      enable = true,
+      mode = "topline", -- 'cursor' 'topline'
     })
   end,
 }

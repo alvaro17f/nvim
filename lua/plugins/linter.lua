4b7046c359150b82328fe4e9dee9b71594fc310a
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-01-14 12:20:43 +0100

    feat(linter): only run the linter in buffers you can modify

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -1,10 +1,7 @@
 -- https://github.com/mfussenegger/nvim-lint?tab=readme-ov-file#available-linters
 return {
   "mfussenegger/nvim-lint",
-  event = {
-    "BufReadPre",
-    "BufNewFile",
-  },
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
     local lint = require("lint")
 
@@ -18,7 +15,9 @@ return {
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
-        lint.try_lint()
+        if vim.opt_local.modifiable:get() then
+          lint.try_lint()
+        end
       end,
     })
   end,

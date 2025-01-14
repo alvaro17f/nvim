8ffacc3ef0ab7676bb0d924307d4594ebfd88553
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-01-14 16:34:53 +0100

    refactor(todo-comments): update configuration

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -5,8 +5,5 @@ return {
   keys = {
     { mode = "n", "<leader>ft", ":TodoFzfLua<CR>", silent = true, desc = "show TODO" },
   },
-  config = function()
-    local todo_comments = require("todo-comments")
-    todo_comments.setup()
-  end,
+  opts = {},
 }

dcede1f9d3f6a00a27e3e06f499f09551967e98d
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-01-14 10:34:01 +0100

    fix(typescript-tools): add ft to lazy load plugin and configure settings

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -1,19 +1,30 @@
 return {
   "pmizio/typescript-tools.nvim",
+  ft = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
-  config = function()
-    require("typescript-tools").setup({
+  opts = {
+    settings = {
       on_attach = function(client, _)
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
       end,
-      settings = {
-        jsx_close_tag = {
-          enable = true,
-          filetypes = { "javascriptreact", "typescriptreact" },
-        },
+
+      jsx_close_tag = {
+        enable = true,
+        filetypes = { "javascriptreact", "typescriptreact" },
       },
-    })
-  end,
+
+      expose_as_code_action = "all",
+
+      tsserver_file_preferences = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+  },
 }

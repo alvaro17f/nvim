c39ab821ebb95282f98eb2917d95022434b0c447
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-01-16 13:11:17 +0100

    refactor(gitsigns): update navigation functions for hunks to target all
    Replace `prev_hunk` and `next_hunk` commands with `nav_hunk` calls, passing explicit options for better clarity and extensibility.

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -13,33 +13,42 @@ return {
     {
       mode = "n",
       "<leader>g;",
-      ":Gitsigns preview_hunk_inline<CR>",
+      function()
+        require("gitsigns").preview_hunk_inline()
+      end,
       silent = true,
       desc = "Gitsigns preview hunk inline",
     },
     {
       mode = "n",
       "[g",
-      ":Gitsigns prev_hunk<CR>",
+      function()
+        require("gitsigns").nav_hunk("prev", { target = "all" })
+      end,
       silent = true,
       desc = "Gitsigns previous hunk",
     },
     {
       mode = "n",
       "]g",
-      ":Gitsigns next_hunk<CR>",
+      function()
+        require("gitsigns").nav_hunk("next", { target = "all" })
+      end,
       silent = true,
       desc = "Gitsigns next hunk",
     },
     {
       mode = "n",
       "<leader>gb",
-      ":Gitsigns blame_line<CR>",
+      function()
+        require("gitsigns").blame_line()
+      end,
       silent = true,
       desc = "Gitsigns blame line",
     },
   },
   opts = {
+    preview_config = { border = "rounded" },
     current_line_blame = true,
     sign_priority = 0,
     signs = icons,

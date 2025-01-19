8dfdb8ed1d3b4f13897f675615b06095268fbe9b
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-01-19 15:44:02 +0100

    feat(plugins): add incline.nvim to display buffer labels in floating windows

--- lua/plugins/mini-icons.lua
+++ lua/plugins/mini-icons.lua
@@ -1,10 +1,11 @@
 return {
   "echasnovski/mini.icons",
   lazy = true,
-  version = false,
-  config = function()
-    local mini_icons = require("mini.icons")
-    mini_icons.setup()
-    mini_icons.mock_nvim_web_devicons() -- mock nvim-web-devicons
+  opts = {},
+  init = function()
+    package.preload["nvim-web-devicons"] = function()
+      require("mini.icons").mock_nvim_web_devicons()
+      return package.loaded["nvim-web-devicons"]
+    end
   end,
 }

11577178f4466b30b07fb6285cbd73f29d7a487b
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-01-20 16:59:03 +0100

    feat(flags): enhance flag module with lazy loading and priority configuration
    The `flags.nvim` plugin now supports lazy loading and prioritization,
    improving initialization flexibility and performance for users.

--- lua/plugins/flags.lua
+++ lua/plugins/flags.lua
@@ -1,13 +1,22 @@
 return {
   "alvaro17f/flags.nvim",
+  lazy = false,
+  priority = 1000,
   keys = {
     { mode = "n", "<leader>F", "<CMD>Flags<CR>", silent = true, desc = "Open Flags" },
   },
-  opts = {
-    icons = true,
-    flags = {
-      copilot = false,
-      debugger = false,
-    },
-  },
+  config = function()
+    local flags = require("flags")
+    flags.setup({
+      icons = true,
+      flags = {
+        copilot = false,
+        debugger = false,
+      },
+    })
+    ------------------------------------
+    vim.g.copilot = flags.get_flags("copilot") or false
+    vim.g.debugger = flags.get_flags("debugger") or false
+    ------------------------------------
+  end,
 }

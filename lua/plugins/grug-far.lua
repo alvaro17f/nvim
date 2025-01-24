106b2cd5a0956f4ba779686357878e00079e0318
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-01-25 00:20:54 +0100

    fix(grug-far): path not updated on new buffer

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -3,40 +3,32 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   --stylua: ignore
   keys = {
-    { mode = "n", "<leader>r", function() require("grug-far").open() end, desc = "Search and replace" },
-    { mode = "v", "<leader>r", function() require("grug-far").with_visual_selection() end, desc = "Search and replace selection" },
+    { mode = "n", "<leader>r", function() require("grug-far").open({ prefills = { paths = vim.fn.expand("%") } }) end, desc = "Search and replace" },
+    { mode = "v", "<leader>r", function() require("grug-far").with_visual_selection({ prefills = { paths = vim.fn.expand("%") } }) end, desc = "Search and replace selection" },
   },
-  config = function()
-    local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
-
-    require("grug-far").setup({
-      engine = "ripgrep", -- 'astgrep'
-      headerMaxWidth = 80,
-      transient = true,
-      prefills = {
-        paths = vim.fn.expand("%"),
-        filesFilter = ext and ext ~= "" and "*." .. ext or nil,
-      },
-      keymaps = {
-        replace = { n = "<localleader><enter>" },
-        qflist = { n = "<c-q>" },
-        syncLocations = { n = "<localleader>s" },
-        syncLine = { n = "<localleader>l" },
-        close = { n = "q" },
-        historyOpen = { n = "<localleader>t" },
-        historyAdd = { n = "<localleader>a" },
-        refresh = { n = "<localleader>f" },
-        openLocation = { n = "<localleader>o" },
-        openNextLocation = { n = "<down>" },
-        openPrevLocation = { n = "<up>" },
-        gotoLocation = { n = "<enter>" },
-        pickHistoryEntry = { n = "<enter>" },
-        abort = { n = "<localleader>b" },
-        help = { n = "g?" },
-        toggleShowCommand = { n = "<localleader>p" },
-        swapEngine = { n = "<localleader>e" },
-        previewLocation = { n = "<localleader>i" },
-      },
-    })
-  end,
+  opts = {
+    engine = "ripgrep", -- 'astgrep'
+    headerMaxWidth = 80,
+    transient = true,
+    keymaps = {
+      replace = { n = "<localleader><enter>" },
+      qflist = { n = "<c-q>" },
+      syncLocations = { n = "<localleader>s" },
+      syncLine = { n = "<localleader>l" },
+      close = { n = "q" },
+      historyOpen = { n = "<localleader>t" },
+      historyAdd = { n = "<localleader>a" },
+      refresh = { n = "<localleader>f" },
+      openLocation = { n = "<localleader>o" },
+      openNextLocation = { n = "<down>" },
+      openPrevLocation = { n = "<up>" },
+      gotoLocation = { n = "<enter>" },
+      pickHistoryEntry = { n = "<enter>" },
+      abort = { n = "<localleader>b" },
+      help = { n = "g?" },
+      toggleShowCommand = { n = "<localleader>p" },
+      swapEngine = { n = "<localleader>e" },
+      previewLocation = { n = "<localleader>i" },
+    },
+  },
 }

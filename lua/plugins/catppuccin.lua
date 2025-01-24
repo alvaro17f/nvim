8d071f079e28a3f974c68e53cac7ea6eb73631de
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-01-24 22:01:12 +0100

    feat(catppuccin): integrate additional plugin support
    Expand theme integrations to include tools like `gitsigns`, `lsp_trouble`, `treesitter_context`, and `which_key`, improving UI consistency across plugins.

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,9 +3,35 @@ return {
   name = "catppuccin",
   lazy = false,
   priority = 1000,
-  config = function()
-    vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
-    require("catppuccin").setup()
+  opts = {
+    flavour = "mocha",
+    transparent_background = true,
+
+    integrations = {
+      blink_cmp = true,
+      diffview = true,
+      gitsigns = true,
+      lsp_trouble = true,
+      markdown = true,
+      mason = true,
+      native_lsp = {
+        enabled = true,
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
+      neotest = true,
+      semantic_tokens = true,
+      snacks = true,
+      treesitter_context = true,
+      which_key = true,
+    },
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
     vim.cmd.colorscheme("catppuccin")
   end,
 }

fe6a84d14e81a3ecaadaa2513a9f9c0200b89950
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-01-28 17:08:41 +0100

    feat(codecompanion): extend flag options to include OpenRouter and OpenAI
    Enhance "copilot" flag configuration by adding support for OpenRouter and OpenAI. This enables greater flexibility and customization for users opting for different code assistant providers.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,12 +3,81 @@ if not vim.g.copilot then
 end
 
 local plugins = {}
+
 local provider = vim.g.copilot
 
-local code_companion_cmd = function()
+local codecompanion_cmd = function()
   local user_input = vim.fn.input("CodeCompanionCmd: ")
   vim.cmd("CodeCompanionCmd " .. user_input)
 end
+
+local codecompanion = {
+  "olimorris/codecompanion.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  keys = {
+    { mode = "n", "<leader>a", "<CMD>CodeCompanionChat Toggle<CR>", silent = true, desc = "CodeCompanion toggle" },
+    { mode = "v", "<leader>a", "<CMD>CodeCompanionActions<CR>", silent = true, desc = "CodeCompanion actions" },
+    { mode = "n", "<leader>:", codecompanion_cmd, desc = "CodeCompanionCmd" },
+    { mode = "v", "ga", "<CMD>CodeCompanionChat Add<CR>", silent = true, desc = "CodeCompanion add" },
+    {
+      mode = "n",
+      "<leader>gc",
+      "<CMD>CodeCompanion /commit<CR>",
+      silent = true,
+      desc = "CodeCompanion generate commit",
+    },
+  },
+  config = function()
+    require("codecompanion").setup({
+      strategies = {
+        chat = {
+          adapter = provider,
+        },
+        inline = {
+          adapter = provider,
+        },
+        agent = {
+          adapter = provider,
+        },
+      },
+      adapters = {
+        openai = function()
+          return require("codecompanion.adapters").extend("openai", {
+            name = "openai",
+            env = {
+              api_key = "OPENAI_API_KEY",
+            },
+          })
+        end,
+        openrouter = function()
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            name = "openrouter",
+            env = {
+              url = "https://openrouter.ai/api",
+              api_key = "OPENROUTER_API_KEY",
+            },
+            headers = {
+              ["X-Title"] = "Neovim - CodeCompanion",
+              ["HTTP-Referer"] = "https://neovim.io",
+            },
+            schema = {
+              model = {
+                default = "deepseek/deepseek-r1-distill-llama-70b",
+              },
+              temperature = {
+                default = 0.5,
+              },
+            },
+          })
+        end,
+      },
+    })
+  end,
+}
 
 local copilot = {
   "zbirenbaum/copilot.lua",
@@ -51,52 +120,6 @@ local copilot = {
           vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
         end
       end, { desc = "Copilot next suggestion" }),
-    })
-  end,
-}
-
-local codecompanion = {
-  "olimorris/codecompanion.nvim",
-  lazy = false,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  keys = {
-    { mode = "n", "<leader>a", "<CMD>CodeCompanionChat Toggle<CR>", silent = true, desc = "CodeCompanion toggle" },
-    { mode = "v", "<leader>a", "<CMD>CodeCompanionActions<CR>", silent = true, desc = "CodeCompanion actions" },
-    { mode = "n", "<leader>:", code_companion_cmd, desc = "CodeCompanionCmd" },
-    { mode = "v", "ga", "<CMD>CodeCompanionChat Add<CR>", silent = true, desc = "CodeCompanion add" },
-    {
-      mode = "n",
-      "<leader>gc",
-      "<CMD>CodeCompanion /commit<CR>",
-      silent = true,
-      desc = "CodeCompanion generate commit",
-    },
-  },
-  config = function()
-    require("codecompanion").setup({
-      strategies = {
-        chat = {
-          adapter = provider,
-        },
-        inline = {
-          adapter = provider,
-        },
-        agent = {
-          adapter = provider,
-        },
-      },
-      adapters = {
-        openai = function()
-          return require("codecompanion.adapters").extend("openai", {
-            env = {
-              api_key = "OPENAI_API_KEY",
-            },
-          })
-        end,
-      },
     })
   end,
 }

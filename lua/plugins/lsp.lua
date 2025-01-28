65d9587673c158a41753bf0c90130c5bfd1099c0
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-01-28 10:39:50 +0100

    feat(mason): update UI icons for better clarity and aesthetics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,9 +6,9 @@ return {
       opts = {
         ui = {
           icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
+            package_installed = "",
+            package_pending = "",
+            package_uninstalled = "",
           },
         },
       },

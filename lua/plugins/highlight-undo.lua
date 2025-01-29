2b9509fa47ba2ad54d54d9d5901da04cfb190eb5
Gregg Housh <greggatghc@gmail.com>
2025-01-29 15:25:21 -0500

    Add highlight-undo

---
+++ lua/plugins/highlight-undo.lua
@@ -0,0 +1,12 @@
+return {
+  "tzachar/highlight-undo.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("highlight-undo").setup({
+      leader_key = ";", -- Recommended to be a single key
+      buffer_leader_key = "m", -- Per Buffer Mappings
+      show_icons = true,
+      global_bookmarks = true,
+    })
+  end,
+}

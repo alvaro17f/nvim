7aa5a9577928238de796fe3258253eb46c0e1226
Gregg Housh <greggatghc@gmail.com>
2025-01-29 14:57:08 -0500

    Add hardtime to make sure I work more efficiently

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,22 @@
+return {
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {
+    disabled_filetypes = {
+      "qf",
+      "netrw",
+      "NvimTree",
+      "lazy",
+      "mason",
+      "grug-far",
+      "lazydo",
+      "lazygit",
+      "minifiles",
+      "snacks_dashboard",
+      "snacks_terminal",
+      "snacks_picker_input",
+      "yazi",
+      "trouble",
+    },
+  },
+}

03095261f222939b77b5f6f77f3600d191d0b0bb
Gregg Housh <greggatghc@gmail.com>
2025-01-29 18:49:53 -0500

    Fix for mouse support in hardtime.nvim

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -2,6 +2,7 @@ return {
   "m4xshen/hardtime.nvim",
   dependencies = { "MunifTanjim/nui.nvim" },
   opts = {
+    disable_mouse = false,
     disabled_keys = {
       ["<Up>"] = {},
       ["<Down>"] = {},

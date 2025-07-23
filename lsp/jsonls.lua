f75a6d75a77cf5fc15fa4476cc025d362c43c632
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-07-23 13:10:47 +0200

    feat(lsp): add JSON schema validation for common config files

--- lsp/jsonls.lua
+++ lsp/jsonls.lua
@@ -5,4 +5,38 @@ return {
   init_options = {
     provideFormatter = true,
   },
+  settings = {
+    json = {
+      schemas = {
+        {
+          fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+          url = "https://json.schemastore.org/babelrc.json",
+        },
+        {
+          fileMatch = { ".eslintrc", ".eslintrc.json" },
+          url = "https://json.schemastore.org/eslintrc.json",
+        },
+        {
+          fileMatch = { "manifest.json", "manifest.webmanifest" },
+          url = "https://json.schemastore.org/web-manifest-combined.json",
+        },
+        {
+          fileMatch = { "package.json" },
+          url = "https://json.schemastore.org/package.json",
+        },
+        {
+          fileMatch = {
+            ".prettierrc",
+            ".prettierrc.json",
+            "prettier.config.json",
+          },
+          url = "https://json.schemastore.org/prettierrc.json",
+        },
+        {
+          fileMatch = { "tsconfig*.json" },
+          url = "https://json.schemastore.org/tsconfig.json",
+        },
+      },
+    },
+  },
 }

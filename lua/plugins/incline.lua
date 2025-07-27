98905f9306c69355a63d64c4cc793a4413405b55
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-07-27 18:44:07 +0200

    feat: enhance incline plugin to show contextual file paths

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -1,3 +1,8 @@
+local contextual_filenames = {
+  "index",
+  "style",
+}
+
 return {
   "b0o/incline.nvim",
   event = { "BufReadPre" },
@@ -5,6 +10,15 @@ return {
   config = function()
     local helpers = require("incline.helpers")
     local devicons = require("nvim-web-devicons")
+    local should_show_context = function(filename)
+      for _, pattern in ipairs(contextual_filenames) do
+        if filename:match(pattern) then
+          return true
+        end
+      end
+      return false
+    end
+
     require("incline").setup({
       window = {
         padding = 0,
@@ -12,7 +26,17 @@ return {
       },
       render = function(props)
         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-        if filename == "" then
+        if should_show_context(filename) then
+          local filepath = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":p")
+          local path_parts = {}
+
+          for part in filepath:gmatch("[^/]+") do
+            table.insert(path_parts, part)
+          end
+          if #path_parts > 1 then
+            filename = path_parts[#path_parts - 1] .. "/" .. filename
+          end
+        elseif filename == "" then
           filename = "[No Name]"
         end
         local ft_icon, ft_color = devicons.get_icon_color(filename)

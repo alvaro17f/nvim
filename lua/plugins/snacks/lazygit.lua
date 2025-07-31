9bd3b967cd7b406d50bbb5ca8861063bdc27d8e2
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-07-31 17:45:33 +0200

    refactor(snacks/lazygit): drop custom styling in favor of defaults

--- lua/plugins/snacks/lazygit.lua
+++ lua/plugins/snacks/lazygit.lua
@@ -37,18 +37,6 @@ return {
           height = 0,
         },
       },
-      theme = {
-        [241] = { fg = "Special" },
-        defaultFgColor = { fg = "Normal" },
-        activeBorderColor = { fg = "DiagnosticError", bold = true }, -- { fg = "Function", bold = true },
-        inactiveBorderColor = { fg = "Comment" },
-        optionsTextColor = { fg = "Function" },
-        selectedLineBgColor = { bg = "Visual" },
-        unstagedChangesColor = { fg = "DiagnosticError" },
-        cherryPickedCommitBgColor = { bg = "default" },
-        cherryPickedCommitFgColor = { fg = "Identifier" },
-        searchingActiveBorderColor = { fg = "MatchParen", bold = true },
-      },
     },
   },
 }

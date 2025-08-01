824bdfe5e9973438c6cc22715218f2a199172bb7
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-08-01 23:24:27 +0200

    feat(themes): add float window transparency and cmp border highlights to catppuccin

--- lua/plugins/themes/catppuccin.lua
+++ lua/plugins/themes/catppuccin.lua
@@ -1,3 +1,4 @@
+-- https://github.com/catppuccin/nvim#configuration
 return {
   "catppuccin/nvim",
   name = "catppuccin",
@@ -6,6 +7,17 @@ return {
   opts = {
     flavour = "mocha",
     transparent_background = true,
+    float = {
+      transparent = true,
+      solid = true,
+    },
+    custom_highlights = function(colors)
+      return {
+        BlinkCmpMenuBorder = { fg = colors.blue },
+        BlinkCmpDocBorder = { fg = colors.sapphire },
+        BlinkCmpSignatureHelpBorder = { fg = colors.blue },
+      }
+    end,
     auto_integrations = true,
     integrations = {
       native_lsp = {

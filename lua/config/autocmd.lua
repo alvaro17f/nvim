dffb0f8bb2be6c38374f96e4cf85f0393802db59
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-08-05 14:46:12 +0200

    refactor(autocmd): cleanup yank highlight autocmd configuration

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -11,10 +11,8 @@ vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
 -- HIGHLIGHT ON YANK
 ------------------------------------
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.highlight.on_yank({ timeout = 100 })
   end,
 })
 

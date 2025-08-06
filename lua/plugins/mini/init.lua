cc9d5cc7aadfac9c6e141b7ee0def401ceec4f5d
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-08-06 12:34:40 +0200

    feat: add require_modules util

--- lua/plugins/mini/init.lua
+++ lua/plugins/mini/init.lua
@@ -1,17 +1,1 @@
-local plugins = {}
-
-local files = vim.fn.readdir(vim.fn.stdpath("config") .. "/lua/plugins/mini/")
-local lua_files = {}
-for _, file in ipairs(files) do
-  if file:match("%.lua$") then
-    table.insert(lua_files, file)
-  end
-end
-
-for _, file in ipairs(lua_files) do
-  if file ~= "init.lua" then
-    table.insert(plugins, require("plugins.mini." .. file:gsub("%.lua$", "")))
-  end
-end
-
-return plugins
+return { require("utils.core").require_modules() }
---
+++ lua/utils/core.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+function M.require_modules(path)
+  local modules = {}
+  local modules_path = path or debug.getinfo(2, "S").source:sub(2):match("(.*/)")
+  local folder_name = modules_path:match("([^/]+)/?$")
+
+  local success, files = pcall(vim.fn.readdir, modules_path)
+  if not success then
+    return modules
+  end
+
+  for _, module in pairs(files) do
+    if module:match("%.lua$") and not module:match("init.lua$") then
+      table.insert(modules, require("plugins." .. folder_name .. "." .. module:gsub("%.lua$", "")))
+    end
+  end
+
+  return modules
+end
+
+return M

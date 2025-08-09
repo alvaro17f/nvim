4067319c55de0cccb5b48458b53a0dc4090fef15
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-08-09 13:55:30 +0200

    feat(opencode): update config

--- lua/plugins/ai/opencode.lua
+++ lua/plugins/ai/opencode.lua
@@ -1,28 +1,21 @@
 return {
   "NickvanDyke/opencode.nvim",
   opts = {
-    provider_id = "openrouter",
-    model_id = vim.env.OPENROUTER_MODEL,
-    port = nil,
-    auto_reload = true,
     terminal = {
       win = {
         enter = true,
-        position = "right",
       },
     },
   },
   -- stylua: ignore
   keys = {
-    { '<C-/>', function() require('opencode').toggle() end, desc = 'Toggle embedded opencode', mode = { 'n', 't' }, },
-    { '<leader>oa', function() require('opencode').ask() end, desc = 'Ask opencode', mode = { 'n', 'v' }, },
-    { '<leader>oA', function() require('opencode').ask('@file ') end, desc = 'Ask opencode about current file', mode = { 'n', 'v' }, },
-    { '<leader>on', function() require('opencode').create_session() end, desc = 'New session', },
-    { '<leader>oe', function() require('opencode').prompt('Explain @cursor and its context') end, desc = 'Explain code near cursor', },
-    { '<leader>or', function() require('opencode').prompt('Review @file for correctness and readability') end, desc = 'Review file', },
-    { '<leader>of', function() require('opencode').prompt('Fix these @diagnostics') end, desc = 'Fix errors', },
-    { '<leader>oo', function() require('opencode').prompt('Optimize @selection for performance and readability') end, desc = 'Optimize selection', mode = 'v', },
-    { '<leader>od', function() require('opencode').prompt('Add documentation comments for @selection') end, desc = 'Document selection', mode = 'v', },
-    { '<leader>ot', function() require('opencode').prompt('Add tests for @selection') end, desc = 'Test selection', mode = 'v', },
+    { '<C-/>',      function() require('opencode').toggle() end, desc = 'Toggle embedded opencode', mode = { 'n', 't' }, },
+    { '<leader>oa', function() require('opencode').ask() end, desc = 'Ask opencode', mode = 'n', },
+    { '<leader>oa', function() require('opencode').ask('@selection: ') end, desc = 'Ask opencode about selection', mode = 'v', },
+    { '<leader>op', function() require('opencode').select_prompt() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
+    { '<leader>on', function() require('opencode').command('session_new') end, desc = 'New session', },
+    { '<leader>oy', function() require('opencode').command('messages_copy') end, desc = 'Copy last message', },
+    { '<S-C-u>',    function() require('opencode').command('messages_half_page_up') end, desc = 'Scroll messages up', mode = 't' },
+    { '<S-C-d>',    function() require('opencode').command('messages_half_page_down') end, desc = 'Scroll messages down', mode = 't' },
   },
 }

41e84e2e2133ef122e24da8bafcf995293cb61f0
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-08-14 10:10:46 +0200

    feat(themes): add everforest

---
+++ lua/plugins/themes/everforest.lua
@@ -0,0 +1,37 @@
+return {
+  "neanias/everforest-nvim",
+  version = false,
+  lazy = false,
+  priority = 1000, -- make sure to load this before all the other start plugins
+  opts = {
+    background = "hard", -- "soft", "medium" or "hard"
+    transparent_background_level = 2, -- 0 to 2
+    italics = true,
+    disable_italic_comments = false,
+    sign_column_background = "none", -- "none" or "grey"
+    ui_contrast = "low", -- "low" or "high"
+    dim_inactive_windows = false,
+    diagnostic_text_highlight = false,
+    diagnostic_virtual_text = "coloured", -- "coloured" or "grey"
+    diagnostic_line_highlight = false,
+    spell_foreground = false,
+    show_eob = false,
+    float_style = "bright", -- "bright" or "dim"
+    inlay_hints_background = "none", -- "none" or "dimmed"
+
+    ---@param hl Highlights
+    ---@param palette Palette
+    on_highlights = function(hl, palette)
+      hl.NormalFloat = { fg = palette.fg, bg = palette.none }
+      hl.FloatBorder = { fg = palette.fg, bg = palette.none }
+      hl.CursorLineNr = { fg = palette.yellow }
+    end,
+
+    ---@param palette Palette
+    colours_override = function(palette) end,
+  },
+  config = function(_, opts)
+    require("everforest").setup(opts)
+    vim.cmd.colorscheme("everforest")
+  end,
+}

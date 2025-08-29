e8c2da7e072cea6833a371fd61449861c2ce7f4f
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-08-29 14:48:57 +0200

    feat(debugger): migrate from nvim-dap-ui to debugmaster.nvim
    Replace nvim-dap-ui and nvim-nio with debugmaster.nvim and one-small-step-for-vimkind
    Introduce unified debug mode toggle with single keybinding

--- plugin/debugger.lua
+++ plugin/debugger.lua
@@ -6,35 +6,21 @@ end
 
 vim.pack.add({
   "https://github.com/nvim-lua/plenary.nvim",
-  "https://github.com/rcarriga/nvim-dap-ui",
-  "https://github.com/nvim-neotest/nvim-nio",
+  "https://github.com/jbyuki/one-small-step-for-vimkind",
   "https://github.com/theHamsta/nvim-dap-virtual-text",
+  "https://github.com/miroshQa/debugmaster.nvim",
   "https://github.com/mfussenegger/nvim-dap",
 }, { load = true, confirm = false })
 
-local dap = require("dap")
-local dapui = require("dapui")
-
-require("utils.debugger").debugger_icons()
-
-require("nvim-dap-virtual-text").setup()
-
-dapui.setup()
-
-dap.listeners.before.attach.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.launch.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
-end
-
+local dm = require("debugmaster")
 local utils = require("utils")
+local utils_debugger = require("utils.debugger")
+local virtual_text = require("nvim-dap-virtual-text")
+
+utils_debugger.debugger_icons()
+virtual_text.setup({})
+
+dm.plugins.osv_integration.enabled = true
 
 local path = vim.fn.stdpath("config") .. "/lua/plugins/debugger/"
 
@@ -49,17 +35,5 @@ vscode.json_decode = function(str)
   return vim.json.decode(json.json_strip_comments(str))
 end
 ----------------------------
-
---stylua: ignore start
-  vim.keymap.set("n", "<Leader>db", function() require("dap").toggle_breakpoint() end, { desc = "Debugger: breakpoint" })
-  vim.keymap.set("n", "<Leader>dc", function() require("dap").continue() end, { desc = "Debugger: run/continue" })
-  vim.keymap.set("n", "<leader>di", function() require("dap").step_into() end, { desc = "Debugger: step into" })
-  vim.keymap.set("n", "<leader>dj", function() require("dap").down() end, { desc = "Debugger: down" })
-  vim.keymap.set("n", "<leader>dk", function() require("dap").up() end, { desc = "Debugger: up" })
-  vim.keymap.set("n", "<leader>dl", function() require("dap").run_last() end, { desc = "Debugger: run last" })
-  vim.keymap.set("n", "<leader>do", function() require("dap").step_out() end, { desc = "Debugger: step out" })
-  vim.keymap.set("n", "<leader>dO", function() require("dap").step_over() end, { desc = "Debugger: step over" })
-  vim.keymap.set("n", "<leader>dP", function() require("dap").pause() end, { desc = "Debugger: pause" })
-  vim.keymap.set("n", "<Leader>dr", function() require("dap").repl.toggle({ height = 15 }) end, { desc = "Debugger: REPL toggle" })
-  vim.keymap.set("n", "<Leader>du", function() require("dapui").toggle() end, { desc = "Debugger: UI toggle" })
---stylua : ignore end
+vim.keymap.set({ "n", "v" }, "<leader>d", dm.mode.toggle, { nowait = true })
+vim.keymap.set("t", "<C-\\>", "<C-\\><C-n>", { desc = "Exit terminal mode" })

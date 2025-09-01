19c4d12da9d5b800f28676ebf3b639ff81cbb9e3
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-09-01 15:49:27 +0200

    feat(lsp): add Lua runtime configuration and workspace settings for LuaLS

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -2,12 +2,27 @@
 return {
   settings = {
     Lua = {
+      runtime = {
+        version = "LuaJIT",
+        path = vim.split(package.path, ";"),
+      },
       diagnostics = {
         enable = true,
         globals = {
           "vim",
           "Snacks",
         },
+      },
+      workspace = {
+        library = {
+          vim.env.VIMRUNTIME,
+          "${3rd}/luv/library",
+          "${3rd}/busted/library",
+        },
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
       },
     },
   },

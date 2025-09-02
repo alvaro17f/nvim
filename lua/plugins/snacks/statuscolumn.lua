0640e25df14276b2fc48802d056d05964becc925
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-09-02 10:14:16 +0200

    feat(statuscolumn): add fold configuration options

--- lua/plugins/snacks/statuscolumn.lua
+++ lua/plugins/snacks/statuscolumn.lua
@@ -1,7 +1,13 @@
 local M = {}
 
 M.config = {
-  statuscolumn = { enabled = true },
+  statuscolumn = {
+    enabled = true,
+    folds = {
+      open = true, -- show open fold icons
+      git_hl = false, -- use Git Signs hl for fold icons
+    },
+  },
 }
 
 return M

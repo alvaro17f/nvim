74b2ae6a7de41b92dce478b677f8afa317b5c879
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-09-03 14:08:03 +0200

    refactor(lsp): simplify lua-ls setup by relying on server defaults

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -2,22 +2,6 @@
 return {
   settings = {
     Lua = {
-      runtime = {
-        version = "LuaJIT",
-      },
-      diagnostics = {
-        enable = true,
-        globals = {
-          "vim",
-        },
-      },
-      workspace = {
-        checkThirdParty = false,
-        library = {
-          vim.env.VIMRUNTIME,
-          vim.fn.stdpath("config"),
-        },
-      },
       telemetry = {
         enable = false,
       },

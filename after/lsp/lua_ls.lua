30ad8ba1a07f4200bd8fbb4de1710f410f313e98
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-09-03 13:33:17 +0200

    feat(lsp): add lua-ls configuration fix(lsp): define vim global for diagnostics refactor(lsp): structure settings hierarchically

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -1,2 +1,26 @@
 ---@type vim.lsp.Config
-return {}
+return {
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        enable = true,
+        globals = {
+          "vim",
+        },
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          vim.fn.stdpath("config"),
+        },
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}

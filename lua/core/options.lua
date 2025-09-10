26afdd691d5703a0be6897a262f90402562a67b7
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-09-10 14:02:45 +0200

    feat(options): enable filetype plugin and indent support

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -8,6 +8,8 @@ vim.g.autocomplete = false
 vim.g.autosave = true
 vim.g.inlayhints = false
 ------------------------------------
+vim.cmd.filetype("plugin indent on")
+------------------------------------
 vim.o.autochdir = true
 vim.o.autocomplete = false
 vim.o.autoread = true
@@ -17,8 +19,9 @@ vim.o.backup = false
 vim.o.breakindent = true
 vim.o.cmdheight = 0
 vim.o.colorcolumn = "80"
-vim.o.complete = "o,.,w,b,u"
-vim.o.completeopt = "fuzzy,menu,menuone,noinsert,noselect,popup"
+vim.o.complete = ".,b,o,u,w"
+vim.o.completefuzzycollect = "keyword,files,whole_line"
+vim.o.completeopt = "fuzzy,menuone,noselect,popup"
 vim.o.cursorline = true
 vim.o.expandtab = true
 vim.o.hidden = true
@@ -30,6 +33,7 @@ vim.o.laststatus = 3
 vim.o.list = true
 vim.o.mouse = "a"
 vim.o.number = true
+vim.o.pummaxwidth = 100
 vim.o.relativenumber = true
 vim.o.scrolloff = 999
 vim.o.shiftround = true
@@ -48,6 +52,8 @@ vim.o.termguicolors = true
 vim.o.timeoutlen = 300
 vim.o.undofile = true
 vim.o.updatetime = 250
+vim.o.wildmode = "noselect:lastused"
+vim.o.wildoptions = "pum,fuzzy"
 vim.o.winborder = "rounded"
 vim.o.wrap = false
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }

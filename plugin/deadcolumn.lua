005c325e71bd032a7346536cc6195b4d9e5074c5
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-09-11 18:10:38 +0200

    feat: replace hardcoded color with dynamic error color in deadcolumn

--- plugin/deadcolumn.lua
+++ plugin/deadcolumn.lua
@@ -1,12 +1,14 @@
 vim.pack.add({ "https://github.com/Bekaboo/deadcolumn.nvim" }, { load = true, confirm = false })
 
+local color = require("utils").color
+
 require("deadcolumn").setup({
   blending = {
-    hlgroup = { "DiffText", "bg" }, --:help highlight-gruoups
+    hlgroup = { "Whitespace", "fg" }, --:help highlight-gruoups
     threshold = 0.75,
   },
   warning = {
     alpha = 0.9,
-    colorcode = "#b3003f",
+    colorcode = color("Error"),
   },
 })

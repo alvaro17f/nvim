07791971559da0c5c18c9656d63acd6cf70ac8eb
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-09-11 22:14:06 +0200

    feat(lsp): enhance jsonls configuration with descriptive schema mappings

--- after/lsp/jsonls.lua
+++ after/lsp/jsonls.lua
@@ -4,22 +4,27 @@ return {
     json = {
       schemas = {
         {
+          description = "Babel",
           fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
           url = "https://json.schemastore.org/babelrc.json",
         },
         {
+          description = "ESLint",
           fileMatch = { ".eslintrc", ".eslintrc.json" },
           url = "https://json.schemastore.org/eslintrc.json",
         },
         {
+          description = "Manifest",
           fileMatch = { "manifest.json", "manifest.webmanifest" },
           url = "https://json.schemastore.org/web-manifest-combined.json",
         },
         {
+          description = "Package.json",
           fileMatch = { "package.json" },
           url = "https://json.schemastore.org/package.json",
         },
         {
+          description = "Prettier",
           fileMatch = {
             ".prettierrc",
             ".prettierrc.json",
@@ -28,6 +33,7 @@ return {
           url = "https://json.schemastore.org/prettierrc.json",
         },
         {
+          description = "TSConfig",
           fileMatch = { "tsconfig*.json" },
           url = "https://json.schemastore.org/tsconfig.json",
         },

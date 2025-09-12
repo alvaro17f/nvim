11a4a3e09b0e4979f58c08e77eb8722910e3fdd9
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-09-12 23:39:54 +0200

    feat: switch autopairs plugin from nvim-autopairs to mini.pairs

---
+++ lua/plugins/mini/pairs.lua
@@ -0,0 +1,3 @@
+Pack.add({ "https://github.com/nvim-mini/mini.pairs" })
+
+require("mini.pairs").setup()
--- plugin/autopairs.lua
+++
@@ -1,7 +0,0 @@
-Pack.add({ "https://github.com/windwp/nvim-autopairs" })
-
-vim.api.nvim_create_autocmd("InsertEnter", {
-  callback = function()
-    require("nvim-autopairs").setup()
-  end,
-})

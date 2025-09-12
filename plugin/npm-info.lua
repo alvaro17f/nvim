b33df6c245e42d0c95cf3eb78a9e5c79c830c786
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-09-12 12:23:18 +0200

    feat(npm-info): remove unnecessary lazy-load logic

--- plugin/npm-info.lua
+++ plugin/npm-info.lua
@@ -1,8 +1,3 @@
 vim.pack.add({ "https://github.com/pxnditxyr/npm-info.nvim" }, { load = true, confirm = false })
 
-vim.api.nvim_create_autocmd("BufReadPre", {
-  pattern = { "package.json" },
-  callback = function()
-    require("npm-info").setup()
-  end,
-})
+require("npm-info").setup()

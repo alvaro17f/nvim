63d662475f7578d2644883c47d0e5c57fcee3022
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-09-12 23:32:09 +0200

    feat(arrow): enable global bookmarks

--- plugin/arrow.lua
+++ plugin/arrow.lua
@@ -1,10 +1,12 @@
 Pack.add({ "https://github.com/otavioschwanck/arrow.nvim" })
 
+local global_bookmarks = true
+
 require("arrow").setup({
   show_icons = true,
-  leader_key = ";", -- Recommended to be a single key
-  buffer_leader_key = "m", -- Per Buffer Mappings
-  global_bookmarks = false, -- if true, arrow will save files globally (ignores separate_by_branch)
-  save_key = "git_root", -- git_root, git_bare, or cwd
+  leader_key = ";",
+  buffer_leader_key = "m",
+  global_bookmarks = global_bookmarks,
+  save_key = global_bookmarks and "cwd" or "git_root",
   separate_by_branch = true,
 })

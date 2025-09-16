fb7eb1a5dfd80ef92b21fce96a4c6a95cdc33ebb
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-09-16 17:11:48 +0200

    fix: lazydev

--- lua/plugins/lazydev/init.lua
+++ lua/plugins/lazydev/init.lua
@@ -1,15 +1,10 @@
 Pack.add({ "https://github.com/folke/lazydev.nvim" })
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "lua" },
-  callback = function()
-    local lazydev = require("lazydev")
+local lazydev = require("lazydev")
 
-    lazydev.setup({
-      library = {
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        { path = "snacks.nvim", words = { "Snacks" } },
-      },
-    })
-  end,
+lazydev.setup({
+  library = {
+    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    { path = "snacks.nvim", words = { "Snacks" } },
+  },
 })

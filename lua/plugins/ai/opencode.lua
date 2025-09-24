712c2d81122a4ed195b0d58819e72303f5f1c6c7
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-09-24 13:51:45 +0200

    feat(opencode): update plugin keybindings

--- lua/plugins/ai/opencode.lua
+++ lua/plugins/ai/opencode.lua
@@ -17,15 +17,13 @@ vim.g.opencode_opts = {
 }
 
 --stylua: ignore start
-  vim.keymap.set({ "n", "t" }, "<C-/>", function() require("opencode").toggle() end, { desc = "Toggle embedded opencode" })
-  vim.keymap.set("n", "<leader>oa", function() require("opencode").ask() end, { desc = "Ask opencode" })
-  vim.keymap.set("v", "<leader>oa", function() require("opencode").ask("@selection: ") end, { desc = "Ask opencode about selection" })
-  vim.keymap.set("n", "<leader>ob", function() require("opencode").ask("@buffer: ") end, { desc = "Ask opencode about current buffer" })
-  vim.keymap.set("n", "<leader>oc", function() require("opencode").ask("@cursor: ") end, { desc = "Ask opencode about current cursor" })
-  vim.keymap.set({"n", "v"}, "<leader>os", function() require("opencode").select() end, { desc = "Select prompt" })
-  vim.keymap.set("n", "<leader>on", function() require("opencode").command("session_new") end, { desc = "New session", })
-  vim.keymap.set("n", "<leader>oy", function() require("opencode").command("messages_copy") end, { desc = "Copy last message", })
-  vim.keymap.set("t", "<S-C-u>", function() require("opencode").command("messages_half_page_up") end, { desc = "Scroll messages up" })
-  vim.keymap.set("t", "<S-C-d>", function() require("opencode").command("messages_half_page_down") end, { desc = "Scroll messages down" })
-  vim.keymap.set('n', '<leader>oe', function() require('opencode').prompt("Explain @cursor and its context") end, { desc = "Explain code near cursor" })
+  vim.keymap.set({ "n", "t" }, "<c-a>", function() require("opencode").toggle() end, { desc = "Toggle embedded opencode" })
+  vim.keymap.set("n", "<leader>aa", function() require("opencode").ask() end, { desc = "Ask opencode" })
+  vim.keymap.set("v", "<leader>aa", function() require("opencode").ask("@selection: ") end, { desc = "Ask opencode about selection" })
+  vim.keymap.set("n", "<leader>ab", function() require("opencode").ask("@buffer: ") end, { desc = "Ask opencode about current buffer" })
+  vim.keymap.set("n", "<leader>ac", function() require("opencode").ask("@cursor: ") end, { desc = "Ask opencode about current cursor" })
+  vim.keymap.set({"n", "v"}, "<leader>as", function() require("opencode").select() end, { desc = "Select prompt" })
+  vim.keymap.set("n", "<leader>an", function() require("opencode").command("session_new") end, { desc = "New session", })
+  vim.keymap.set("n", "<leader>ay", function() require("opencode").command("messages_copy") end, { desc = "Copy last message", })
+  vim.keymap.set('n', '<leader>ae', function() require('opencode').prompt("Explain @cursor and its context") end, { desc = "Explain code near cursor" })
 --stylua: ignore end

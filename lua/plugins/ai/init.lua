57a1538224432e0cd2bf8a2ed90f3e855f2ad17b
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-09-25 13:32:37 +0200

    feat(ai): dynamically load AI suggestion plugins based on flag value

--- lua/plugins/ai/init.lua
+++ lua/plugins/ai/init.lua
@@ -4,8 +4,8 @@ if Flags.ai_chat then
   table.insert(plugins, require("plugins.ai." .. Flags.ai_chat))
 end
 
-if Flags.ai_suggestions == "neocodeium" then
-  table.insert(plugins, require("plugins.ai.neocodeium"))
+if Flags.ai_suggestions and Flags.ai_suggestions ~= "lsp" then
+  table.insert(plugins, require("plugins.ai." .. Flags.ai_suggestions))
 end
 
 return plugins

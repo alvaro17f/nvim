3dec8d0194a824841b70e06a75e27d592c33af66
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-09-27 23:48:25 +0200

    feat(themes/vague): update highlight groups for better visual consistency

--- lua/plugins/themes/vague.lua
+++ lua/plugins/themes/vague.lua
@@ -1,20 +1,16 @@
 Pack.add({ { src = "https://github.com/vague2k/vague.nvim", name = "vague" } })
 
-vim.o.winborder = "none"
-
 local vague = require("vague")
 
 vague.setup({
   transparent = false,
 
   on_highlights = function(hl, c)
-    hl.WinBar = {
-      bg = c.bg,
-      fg = c.fg,
-    }
-    hl.SnacksIndentScope = {
-      fg = c.string,
-    }
+    hl.FloatBorder = { bg = c.bg, fg = c.comment }
+    hl.SnacksDashboardHeader = { fg = c.type }
+    hl.SnacksIndentScope = { fg = c.string }
+    hl.WinBar = { bg = c.bg, fg = c.fg }
+    hl.WinSeparator = { fg = c.comment }
   end,
 })
 

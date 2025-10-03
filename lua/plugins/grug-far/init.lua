4bddd4f41aa71b43293a817028150d6244633838
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-10-03 09:44:19 +0200

    feat(grug-far): restore default keybindings

--- lua/plugins/grug-far/init.lua
+++ lua/plugins/grug-far/init.lua
@@ -3,29 +3,9 @@ Pack.add({ "https://github.com/MagicDuck/grug-far.nvim" })
 local grug_far = require("grug-far")
 
 grug_far.setup({
-  engine = "ripgrep", -- 'astgrep'
+  engine = "ripgrep",
   headerMaxWidth = 80,
   transient = true,
-  keymaps = {
-    replace = { n = "<localleader><enter>" },
-    qflist = { n = "<c-q>" },
-    syncLocations = { n = "<localleader>s" },
-    syncLine = { n = "<localleader>l" },
-    close = { n = "q" },
-    historyOpen = { n = "<localleader>t" },
-    historyAdd = { n = "<localleader>a" },
-    refresh = { n = "<localleader>f" },
-    openLocation = { n = "<localleader>o" },
-    openNextLocation = { n = "<down>" },
-    openPrevLocation = { n = "<up>" },
-    gotoLocation = { n = "<enter>" },
-    pickHistoryEntry = { n = "<enter>" },
-    abort = { n = "<localleader>b" },
-    help = { n = "g?" },
-    toggleShowCommand = { n = "<localleader>p" },
-    swapEngine = { n = "<localleader>e" },
-    previewLocation = { n = "<localleader>i" },
-  },
 })
 
 vim.keymap.set("n", "<leader>r", function()

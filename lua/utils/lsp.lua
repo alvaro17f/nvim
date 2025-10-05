5341d8ccbdafcd16459f6c4235ac43fe36029f32
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-10-05 14:20:57 +0200

    refactor(lsp): relocate selection range keybindings to unconditional section
    Moves Alt+O and Alt+O keybindings from conditional client support check
    to the main keybinding section. This prevents inconsistent availability
    of text object selection functionality based on server implementation.

--- lua/utils/lsp.lua
+++ lua/utils/lsp.lua
@@ -145,6 +145,14 @@ local on_attach = function()
       opts.desc = "Show documentation for what is under cursor"
       vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
+      vim.keymap.set({ "n", "x" }, "<a-o>", function()
+        vim.lsp.buf.selection_range(vim.v.count1)
+      end, { desc = "Selection range: outer" })
+
+      vim.keymap.set({ "n", "x" }, "<a-i>", function()
+        vim.lsp.buf.selection_range(-vim.v.count1)
+      end, { desc = "Selection range: inner" })
+
       opts.desc = "Restart LSP"
       vim.keymap.set("n", "grl", "<CMD>LspRestart<CR>", opts)
 
@@ -204,16 +212,6 @@ local on_attach = function()
 
       if client:supports_method(methods.textDocument_onTypeFormatting, args.buf) then
         vim.lsp.on_type_formatting.enable(true, { client_id = client.id })
-      end
-
-      if client:supports_method(methods.textDocument_selectionRange, args.buf) then
-        vim.keymap.set({ "n", "x" }, "<a-o>", function()
-          vim.lsp.buf.selection_range(vim.v.count1)
-        end, { desc = "Selection range: outer" })
-
-        vim.keymap.set({ "n", "x" }, "<a-i>", function()
-          vim.lsp.buf.selection_range(-vim.v.count1)
-        end, { desc = "Selection range: inner" })
       end
     end,
   })

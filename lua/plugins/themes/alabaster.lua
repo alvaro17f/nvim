e14a8a75384a1032b820f8b2f2c21a83f3252637
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-12-13 13:16:55 +0100

    feat(themes): add alabaster

---
+++ lua/plugins/themes/alabaster.lua
@@ -0,0 +1,8 @@
+Pack.add({ "https://git.sr.ht/~p00f/alabaster.nvim" })
+
+vim.o.background = "dark"
+vim.g.alabaster_dim_comments = false
+vim.g.alabaster_floatborder = true
+
+vim.cmd.set("termguicolors")
+vim.cmd.colorscheme("alabaster")

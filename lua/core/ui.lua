c19db3c0a09f67ae2e3b8455e8fdf283815eeb88
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2026-02-25 08:59:05 +0100

    feat(ui): configure message display with timeout and target settings

--- lua/core/ui.lua
+++ lua/core/ui.lua
@@ -3,4 +3,11 @@
 ------------------------------------
 local ui = require("vim._core.ui2")
 
-ui.enable({})
+ui.enable({
+  enable = true,
+  msg = {
+    ---@type 'cmd'|'msg'
+    target = "msg",
+    timeout = 4000,
+  },
+})

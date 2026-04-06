dd7db46422c43a6cd10ff330823c7cd7d5fa5559
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2026-04-06 19:30:11 +0200

    feat: add catppuccin-nvim theme support with transparent UI elements

---
+++ lua/plugins/themes/catppuccin-nvim.lua
@@ -0,0 +1,20 @@
+vim.cmd.colorscheme("catppuccin")
+
+vim.g.lualine = "horizon"
+-- vim.o.winborder = "none"
+
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "TabLine", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "TabLineFill", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "TabLineSel", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "Pmenu", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "PmenuSel", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "WinSeparator", { fg = Utils.color("Comment") })

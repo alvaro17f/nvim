b310faef2ed2888fba04967843fdd5befa13a4d5
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2026-05-12 15:58:05 +0200

    fix: ui

--- lua/core/ui.lua
+++ lua/core/ui.lua
@@ -6,8 +6,20 @@ local ui = require("vim._core.ui2")
 ui.enable({
   enable = true,
   msg = {
-    ---@type 'cmd'|'msg'
-    target = "msg",
-    timeout = 4000,
+    ---@type string|table<string, 'cmd'|'msg'|'pager'>
+    targets = "msg",
+    cmd = {
+      height = 0.5,
+    },
+    dialog = {
+      height = 0.5,
+    },
+    msg = {
+      height = 0.5,
+      timeout = 4000,
+    },
+    pager = {
+      height = 1,
+    },
   },
 })
